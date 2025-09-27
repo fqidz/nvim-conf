7c3e020fd820a56bf6d7213a4bf8e36fb9efb0b8
fqidz <faidz.arante@gmail.com>
2025-09-27 23:14:48 +0300

    fix: jdtls

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,29 +1,19 @@
 -- a lot of the following is from:
--- https://github.com/fabelv/nix-config/blob/9871c41701b8f0a0c97cd385ef60e4c94a825e0c/home-manager/features/editors/nvim/plugin/java.lua
+-- https://github.com/LazyVim/LazyVim/blob/b8899781516da71ba0f63afa93fb4a6b25dff144/lua/lazyvim/plugins/extras/lang/java.lua#L87-L145
+
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = os.getenv("JDTLS_WORKSPACE") .. '/' .. project_name
-local config_path = os.getenv("JDTLS_CONFIG")
-local launcher_path = vim.fn.glob(os.getenv("JDTLS_HOME") .. '/plugins/org.eclipse.equinox.launcher_*.jar')
+local jdtls_workspace_dir =
+  vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/workspace"
+local jdtls_config_dir =
+  vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/config"
 
 local config = {
-    -- cmd options from https://github.com/mfussenegger/nvim-jdtls?tab=readme-ov-file#configuration-verbose
-    -- jdtls somehow doesn't detect javafx with just `cmd = { 'jdtls' }`
-    cmd = {
-      'java',
-      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-      '-Dosgi.bundles.defaultStartLevel=4',
-      '-Declipse.product=org.eclipse.jdt.ls.core.product',
-      '-Dlog.protocol=true',
-      '-Dlog.level=ALL',
-      '-Xmx1g',
-      '--add-modules=ALL-SYSTEM',
-      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-      '-jar', launcher_path,
-      '-configuration', config_path,
-      '-data', workspace_dir
-    },
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  cmd = {
+    vim.fn.exepath("jdtls"),
+    '-configuration', config_path,
+    '-data', workspace_dir
+  },
+  root_dir = vim.fs.root(vim.api.nvim_buf_get_name(0), vim.lsp.config.jdtls.root_markers),
 }
 require('jdtls').start_or_attach(config)
 
